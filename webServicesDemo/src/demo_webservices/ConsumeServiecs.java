b88c043ea02a223e52a10b5b5467ce127558c401
abhishek.rana <abhishek.rana@SPICITY1158.SPI.COM>
2017-01-23 12:03:35 +0530

    Adding Comment

---
+++ webServicesDemo/src/demo_webservices/ConsumeServiecs.java
@@ -0,0 +1,11 @@
+package demo_webservices;
+
+import demo_webservices.StockQuoteService;
+
+public class ConsumeServiecs {
+	public static void main(String[] args) {
+		StockQuoteService s = new StockQuoteService();
+		s.sayHello("Abhishek");
+	}//Extra
+
+}
